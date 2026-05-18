60c321202280b9b2ff409c091360bfc7474fcf55
Saurav09s <sauravs19002@gmail.com>
2026-05-18 22:24:20 +0530

    create script for home page and add it to home.ejs

---
+++ Frontend/public/script/home.js
@@ -0,0 +1,39 @@
+window.addEventListener('load', () => {
+  const counterElement = document.getElementById('trained-people');
+  const targetNumber = 100; // The 'n' trained people
+  const duration = 2000; // Animation duration in milliseconds
+  const intervalTime = 20; // Step time
+  
+  let startValue = 0;
+  const increment = targetNumber / (duration / intervalTime);
+
+  const counterInterval = setInterval(() => {
+    startValue += increment;
+    if (startValue >= targetNumber) {
+      counterElement.textContent = targetNumber.toLocaleString();
+      clearInterval(counterInterval);
+    } else {
+      counterElement.textContent = Math.floor(startValue).toLocaleString();
+    }
+  }, intervalTime);
+});
+
+window.addEventListener('load', () => {
+  const counterElement = document.getElementById('students');
+  const targetNumber = 500; // The 'n' trained people
+  const duration = 2000; // Animation duration in milliseconds
+  const intervalTime = 20; // Step time
+  
+  let startValue = 0;
+  const increment = targetNumber / (duration / intervalTime);
+
+  const counterInterval = setInterval(() => {
+    startValue += increment;
+    if (startValue >= targetNumber) {
+      counterElement.textContent = targetNumber.toLocaleString(); // Add commas
+      clearInterval(counterInterval);
+    } else {
+      counterElement.textContent = Math.floor(startValue).toLocaleString();
+    }
+  }, intervalTime);
+});
